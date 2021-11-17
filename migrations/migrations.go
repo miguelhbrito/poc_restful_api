d8644a6f6bb04e1aaa7c33cef2efee1277cd8da3
Miguel Pereira <miguelhbrito@gmail.com>
2021-11-17 11:28:38 -0300

    added feature to check client os, to apply migrations

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,7 +1,10 @@
 package migrations
 
 import (
+	"bufio"
 	"database/sql"
+	"os"
+	"runtime"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -16,21 +19,28 @@ func InitMigrations(db *sql.DB) {
 		panic(err)
 	}
 
-	//Linux users :
-	/*pwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	m, err := migrate.NewWithDatabaseInstance(
-		fmt.Sprintf("file://%s", pwd),
-		"postgres", driver)
-	*/
+	var m *migrate.Migrate
+	if runtime.GOOS == "windows" {
+		fmt.Print("Windows OS detected, please enter project path:")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+		m, err = migrate.NewWithDatabaseInstance(
+			fmt.Sprintf("file://"+text+"stone_assignment/migrations/"),
+			"postgres", driver)
 
-	//Windows users :
-	m, err := migrate.NewWithDatabaseInstance(
-		fmt.Sprintf("file://C:/Users/migue/Documents/dev/github/stone_assignment/migrations/"),
-		"postgres", driver)
+	} else {
+		pwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		m, err = migrate.NewWithDatabaseInstance(
+			fmt.Sprintf("file://%s", pwd),
+			"postgres", driver)
+	}
 
 	m.Down()
 	m.Up()
+
+	fmt.Println("Successfully migrations applied!")
 }
